4c55f7296460d390fbe7c0f06c4207435da03452
1061199028@qq.com <1061199028@qq.com>
2021-03-07 18:29:07 +0800

    add vue document

---
+++ project/myproject/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="messageBox">
+    hello vue  
+    <label>{{message}}</label>
+  </div>
+</template>
+
+
+
+
+
+<script>
+
+export default {
+  data(){
+    return {
+      message:"我是中国人"
+    }
+  }
+}
+
+
+
+</script>
--- project/myproject/src/main.js
+++ project/myproject/src/main.js
@@ -1,20 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import './registerServiceWorker'
+import router from './router'
 import store from './store'
-import router from "./router"
-
-
 
 Vue.config.productionTip = false
 
-
-
 new Vue({
-  router: router,
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
-
